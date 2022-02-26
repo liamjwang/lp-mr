b701594bed93d148baffb049b1da2ceacffc6c33
Liam Wang <5054950+wangl5@users.noreply.github.com>
2022-02-25 19:59:08 -0500

    Add protobuf
    Former-commit-id: 850c4c30f3817d2aaf22de0fc66f7859fde175cd

---
+++ Assets/Scripts/SmoothFollowOther.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollowOther : MonoBehaviour
+{
+
+    public Transform other;
+    
+    private Vector3 velocity = Vector3.zero;
+    private float currVel = 0;
+    public float translateSmoothTime = 0.3F;
+    public float rotateSmoothTime = 0.3F;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform myTransform = transform;
+        myTransform.position = Vector3.SmoothDamp(myTransform.position, other.position, ref velocity, translateSmoothTime);
+        Quaternion transformRotation = myTransform.rotation;
+        float delta = Quaternion.Angle(transformRotation, other.rotation);
+        if (delta > 0f)
+        {
+            float t = Mathf.SmoothDampAngle(delta, 0.0f, ref currVel, rotateSmoothTime);
+            t = 1.0f - (t / delta);
+            myTransform.rotation = Quaternion.Slerp(transformRotation, other.rotation, t);
+        }
+
+        myTransform.localScale = other.localScale;
+    }
+}

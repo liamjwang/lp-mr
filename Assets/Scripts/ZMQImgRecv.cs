e98ec1258bb3a165e78281df0dec13da5a5bee93
Liam Wang <5054950+wangl5@users.noreply.github.com>
2022-09-14 18:55:09 -0400

    Add zmq image recv

---
+++ Assets/Scripts/ZMQImgRecv.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class ZMQImgRecv : MonoBehaviour
+{
+    public MeshFilter targetMesh;
+
+    private Proto.Messages.MeshStamped meshStamped;
+    
+
+    private void Start()
+    {
+        ZMQConnection connect = ZMQConnection.GetOrCreateInstance();
+        connect.Subscribe<CapnpGen.ImageStamped>("live_b_jpeg/", OnImgRecv);
+    }
+
+    private void OnImgRecv(CapnpGen.ImageStamped imgStamped)
+    {
+        byte[] bytes = imgStamped.Image.Data.ToArray();
+        Texture2D tex = new Texture2D(2, 2);
+        tex.LoadImage(bytes);
+        targetMesh.GetComponent<Renderer>().material.mainTexture = tex;
+    }
+}

7c74004b13a4402726e04c169b8cc08ea8eab157
Liam Wang <5054950+wangl5@users.noreply.github.com>
2022-09-16 21:08:58 -0400

    Add skin entry point

---
+++ Assets/SkinEntry.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkinEntry : MonoBehaviour
+{
+
+    public Transform skin;
+
+    public Transform needle;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        new Plane(skin.up, skin.position).Raycast(new Ray(needle.position, needle.forward), out float distance);
+        Vector3 point = needle.position + needle.forward * distance;
+        transform.position = point;
+    }
+}

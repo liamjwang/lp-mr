de8b38951076225ebba4c94771d617dafb13fe82
Liam Wang <5054950+wangl5@users.noreply.github.com>
2022-10-31 22:54:21 -0400

    Add mesh requester

--- Assets/Scripts/ZMQMeshRecv.cs
+++ Assets/Scripts/ZMQMeshRecv.cs
@@ -9,35 +9,65 @@ using UnityEngine;
 public class ZMQMeshRecv : MonoBehaviour
 {
     public MeshFilter targetMesh;
+    
+    public string RequestMeshTopic = "mesh/request/";
 
     
     private Proto.Messages.MeshStamped meshStamped;
     private Mesh myMesh = new Mesh();
+    private Vector3[] lastVertices;
+    private RateLimiter rateLimiter;
+    private ZMQConnection connect;
     
+    private bool hasMesh = false;
+
 
     private void Start()
     {
+        rateLimiter = new RateLimiter(0.2f);
         myMesh = new Mesh();
         targetMesh.mesh = myMesh;
         
-        ZMQConnection connect = ZMQConnection.GetOrCreateInstance();
+        connect = ZMQConnection.GetOrCreateInstance();
         connect.Subscribe<CapnpGen.Mesh>("volume/mesh/", OnMeshRecv);
         
     }
 
+    private void Update()
+    {
+        if (!hasMesh)
+        {
+            rateLimiter.Run(() =>
+            {
+                connect.Publish(RequestMeshTopic, Array.Empty<byte>());
+                Debug.Log("Requesting mesh");
+            });
+        }
+    }
+
     private void OnMeshRecv(CapnpGen.Mesh mesh)
     {
-        myMesh.Clear();
 
         Vector3[] vertices = new Vector3[mesh.Vertices.Count / 3];
         for (int i = 0; i < mesh.Vertices.Count; i += 3)
         {
             vertices[i / 3] = new Vector3(mesh.Vertices[i], mesh.Vertices[i + 1], mesh.Vertices[i + 2]);
         }
+        if (lastVertices != null && lastVertices.SequenceEqual(vertices))
+        {
+            Debug.Log("Same mesh");
+            return;
+        }
+        
+        Debug.Log("New mesh");
 
+        lastVertices = vertices;
+
+        myMesh.Clear();
         myMesh.vertices = vertices;
         myMesh.triangles = mesh.Faces.ToArray();
         myMesh.RecalculateNormals();
+        hasMesh = true;
     }
     //
     // class USMeshServiceImpl : USMeshService.USMeshServiceBase
